028fcf91b9e3e399e6819d36c024ac3c50065a44
C. Titus Brown <titus@idyll.org>
2023-12-18 15:22:59 -0800

    bundle results update - w/sweeps

--- bundle-results.sh
+++ bundle-results.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 zip -r sinks-results-today.zip sink_metag/JRH_{?,??}.*{out,csv} \
     isolates.x.metagenomes.manysearch.csv isolates.x.metagenomes.summary.csv \
+    isolates.x.sweep.manysearch.csv isolates.x.sweep.summary.csv \
     sink_metag/JRH_combined.kreport.csv
